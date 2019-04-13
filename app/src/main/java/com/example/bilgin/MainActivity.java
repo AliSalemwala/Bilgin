b29ba324a82aadc2cb55f9a25e2111783ac45267
Ali Altaf Salemwala <aali_altaf@yahoo.com>
2019-04-13 18:00:28 +0300

    Uploading all files

---
+++ app/src/main/java/com/example/bilgin/MainActivity.java
@@ -0,0 +1,164 @@
+package com.example.bilgin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText _emailText;
+    EditText _passwordText;
+    Button _loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        _emailText = findViewById(R.id.user_id);
+        _passwordText = findViewById(R.id.password);
+        _loginButton = findViewById(R.id.log_in);
+
+        _loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DisplayActivity.class);
+                startActivity(intent);
+                //TODO make the onlick
+            }
+        });
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void onLoginSuccess() {
+        _loginButton.setEnabled(true);
+        finish();
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        _loginButton.setEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // disable going back to the MainActivity
+        moveTaskToBack(true);
+    }
+
+    /*
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+
+
+    public void login() {
+        Log.d(TAG, "Login");
+
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+        _loginButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
+                R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        // TODO: Implement your own authentication logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onLoginSuccess or onLoginFailed
+                        onLoginSuccess();
+                        // onLoginFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_SIGNUP) {
+            if (resultCode == RESULT_OK) {
+
+                // TODO: Implement successful signup logic here
+                // By default we just finish the Activity and log them in automatically
+                this.finish();
+            }
+        }
+    }
+
+
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _emailText.setError("enter a valid email address");
+            valid = false;
+        } else {
+            _emailText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            _passwordText.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            _passwordText.setError(null);
+        }
+
+        return valid;
+    }*/
+}
---
+++ app/src/main/java/com/example/bilgin/com/models/Employee.java
@@ -0,0 +1,127 @@
+package com.example.bilgin.com.models;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+/**
+ * Assumed simulations:
+ * Company,
+ * Utilities,
+ * Visual Credit Card,
+ *
+ */
+
+public class Employee {
+    private String name;
+    private String id;
+    private String company;
+    private float salary;
+    private float debt;
+    private VirtualCard card;
+    private boolean hasCard;
+
+    public Employee(String name, String id, String company, float salary) {
+        this.name = name;
+        this.id = id;
+        this.company = company;
+        this.salary = salary;
+
+        this.debt = 0;
+        this.card = null;
+        this.hasCard = false;
+    }
+
+    //Make Bank Details Class.
+    //Make LoanHistory Class.
+
+    /**
+     * Check if the user is eligible for the requested amount of a loan
+     * @param amountRequested
+     * @return true if eligible, else false
+     */
+    private boolean isEligibleForLoan(float amountRequested) {
+        boolean isEligible = false;
+
+        if (salary - (debt + amountRequested) >= 0) {
+            isEligible = true;
+        }
+
+        return isEligible;
+    }
+
+    /**
+     * Calculate and return the weekly earnings
+     * @return
+     */
+    public float calculateDaily() {
+        String month = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime()).substring(4, 6);
+        int monthNum = Integer.parseInt(month);
+        int days;
+
+        if (monthNum == 2)
+            days =  29;
+        else if (monthNum == 7 || monthNum % 2 == 0)
+            days = 31;
+        else
+            days = 30;
+
+
+        return (salary - debt) / days;
+    }
+
+    /**
+     * Checks if the user is eligible for a monthly loan
+     * @param amountRequested requested amount of the loan
+     * @return true if the requested loan is transferrable, false if not
+     */
+    public boolean requestLoan(int amountRequested) {
+        boolean isEligible = this.isEligibleForLoan(amountRequested);
+
+        if (isEligible) {
+            debt += amountRequested;
+        }
+
+        return isEligible;
+    }
+
+    public boolean payUtilities(float amountToPay) {
+        boolean isEligible = this.isEligibleForLoan(amountToPay);
+
+        if(isEligible)
+            debt += amountToPay;
+
+        return isEligible;
+    }
+
+    public boolean createCard(String id, float limit) {
+        if (hasCard)
+            return false;
+
+        this.card = new VirtualCard(id, limit);
+        return true;
+    }
+
+    public boolean makePayment(float amount)  {
+        if (card == null)
+            return false;
+
+        return card.makePayment(amount);
+    }
+
+
+    public boolean getHasCard() {
+        return hasCard;
+    }
+
+    public VirtualCard getCard() {
+        return card;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
